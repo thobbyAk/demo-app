2af6e3944904a3c315890e611d3984d7c65a50c7
Oluwatobi Akinrinade <tobi@dystopialabs.com>
2024-01-17 13:52:07 +0000

    cleanup app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,1 +1,8 @@
-export {}
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+
+});
